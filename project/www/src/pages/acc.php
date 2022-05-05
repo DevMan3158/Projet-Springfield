39b9005d8dc8c1fdd2f7fe5e500461b6ac12a869
loloxdev <loris_labarre@outlook.fr>
2022-05-05 16:58:22 +0200

    Ajout d'un grid pour l'accueil, petites modiff

--- project/www/src/pages/acc.php
+++ project/www/src/pages/acc.php
@@ -1,1 +1,63 @@
-<p> acceuil </p>
+
+<h3>
+    <div></div>
+        Bienvenue
+    <div></div>
+</h3>
+
+<figure class="figBox">
+    <img src="data/img/krusty-burger.webp" alt="Le restaurant Krusty Burger">
+    <figcaption class="captionBox">
+        Krusty Burger
+    </figcaption>
+</figure>
+
+<figure class="figBox">
+    <img src="data/img/krusty-burger.webp" alt="Le restaurant Krusty Burger">
+    <figcaption class="captionBox">
+        Krusty Burger
+    </figcaption>
+</figure>
+
+<figure class="figBox">
+    <img src="data/img/krusty-burger.webp" alt="Le restaurant Krusty Burger">
+    <figcaption class="captionBox">
+        Krusty Burger
+    </figcaption>
+</figure>
+
+<figure class="figBox">
+    <img src="data/img/krusty-burger.webp" alt="Le restaurant Krusty Burger">
+    <figcaption class="captionBox">
+        Krusty Burger
+    </figcaption>
+</figure>
+
+<figure class="figBox">
+    <img src="data/img/krusty-burger.webp" alt="Le restaurant Krusty Burger">
+    <figcaption class="captionBox">
+        Krusty Burger
+    </figcaption>
+</figure>
+
+<figure class="figBox">
+    <img src="data/img/krusty-burger.webp" alt="Le restaurant Krusty Burger">
+    <figcaption class="captionBox">
+        Krusty Burger
+    </figcaption>
+</figure>
+
+<figure class="figBox">
+    <img src="data/img/krusty-burger.webp" alt="Le restaurant Krusty Burger">
+    <figcaption class="captionBox">
+        Krusty Burger
+    </figcaption>
+</figure>
+
+<figure class="figBox">
+    <img src="data/img/krusty-burger.webp" alt="Le restaurant Krusty Burger">
+    <figcaption class="captionBox">
+        Krusty Burger
+    </figcaption>
+</figure>
+
