57c305b54f0ed1d32020fa0dda4719f18378330d
Ludovic NAULOT <pctronique@free.fr>
2022-05-05 16:58:28 +0200

    créatiioon du bbcode (a mettre en fonctionnement

---
+++ project/www/test_bbcode.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+
+<textarea id="editor"></textarea>
+    
+</body>
+</html>
