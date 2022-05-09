d107b8a7fafcc59e4effbb57dd128f7b04582888
loloxdev <loris_labarre@outlook.fr>
2022-05-09 15:49:53 +0200

    Mise en place d'une jointure pour sql

--- project/www/src/pages/acc.php
+++ project/www/src/pages/acc.php
@@ -1,8 +1,12 @@
 <?php
     include_once dirname(__FILE__) . '/../config/sgbd_config.php';
     $sgbd= connexion_sgbd();
-    $articles = $sgbd->prepare('SELECT titre, src, alt FROM springfield.photos ORDER BY RAND()
-    LIMIT 10');
+    $articles = $sgbd->prepare('SELECT photos.titre, photos.src, photos.alt FROM springfield.photos INNER JOIN springfield.produits ON produits.id_produit = photos.id_produit
+    INNER JOIN categorie ON produits.id_cat = categorie.id_cat WHERE categorie.id_cat !=3  ORDER BY RAND()
+    LIMIT 5');
+    $personnages = $sgbd->prepare('SELECT photos.titre, photos.src, photos.alt FROM springfield.photos INNER JOIN springfield.produits ON produits.id_produit = photos.id_produit
+    INNER JOIN categorie ON produits.id_cat = categorie.id_cat WHERE categorie.id_cat=3  ORDER BY RAND()
+    LIMIT 5');
 
 ?>
 
@@ -18,13 +22,26 @@
 
 <?php
         $articles->execute();
-        $resultat = $articles->fetchAll((PDO::FETCH_ASSOC));
+        $resultat_articles = $articles->fetchAll((PDO::FETCH_ASSOC));
 
-        foreach ($resultat as $article) {
+
+
+        foreach ($resultat_articles as $article) {
 
             echo    '<figure>
                         <img src="data/img/'.($article['src']).'" alt="'.($article['alt']).'">
                         <figcaption>' .($article['titre']). '</figcaption>
+                    </figure>';
+    }
+
+        $personnages->execute();
+        $resultat_personnages = $personnages->fetchAll((PDO::FETCH_ASSOC));
+
+        foreach ($resultat_personnages as $personnage) {
+
+            echo    '<figure>
+                        <img class="contain" src="data/img/'.($personnage['src']).'" alt="'.($personnage['alt']).'">
+                        <figcaption>' .($personnage['titre']). '</figcaption>
                     </figure>';
     }
 ?>
