d0f5e9a7ac580fc442b73285d668e89fbbf994d7
LoloxDev <loris_labarre@outlook.fr>
2022-05-15 22:22:20 +0200

    Mise en place du calcul de page& boucle produits

--- project/www/src/pages/cat.php
+++ project/www/src/pages/cat.php
@@ -13,41 +13,82 @@
 
                             //Ici on récupere le titre des catégories
 
-                    $articles_titre = $sgbd->prepare('SELECT categorie.nom FROM springfield.categorie WHERE categorie.id_cat=:id_cat');
-                    $articles_titre->execute([":id_cat"=>$_GET["cat"]]);
+                            $articles_titre = $sgbd->prepare('SELECT categorie.nom FROM springfield.categorie WHERE categorie.id_cat=:id_cat');
+                            $articles_titre->execute([":id_cat"=>$_GET["cat"]]);
 
-                  if($articles_titre->rowCount() > 0) {
+                            if($articles_titre->rowCount() > 0) {
 
 
-                    echo    '<h3>
-                            '.$articles_titre->fetch((PDO::FETCH_ASSOC))['nom'].'
-                            </h3>';
+                            echo    '<h3>
+                                    '.$articles_titre->fetch((PDO::FETCH_ASSOC))['nom'].'
+                                    </h3>';
+
+                            // On récupere le nombre de produits de la catégories actuelle pour calculer le nombre de pages
+
+                            $nombre_produits = $sgbd->prepare('SELECT id_cat, COUNT(*) AS nbByCat FROM produits WHERE produits.id_cat=:id_cat');
+                            
+                            $nombre_produits->execute([":id_cat"=>$_GET["cat"]]);
+
+                            $nombre_produits_by_idcat = $nombre_produits->fetchAll((PDO::FETCH_ASSOC));
+                            
+                            foreach ($nombre_produits_by_idcat as $nbByCat) {
+                                $nbOfPages = ceil($nbByCat['nbByCat'] / 5);
+                            }
 
                             //Ici on récupere les produits pour la première page
 
-                            $articles1 = $sgbd->prepare('SELECT produits.id_produit, categorie.nom, photos.titre, photos.src, photos.alt FROM springfield.photos INNER JOIN springfield.produits ON produits.id_produit = photos.id_produit
-                            INNER JOIN categorie ON produits.id_cat = categorie.id_cat WHERE categorie.id_cat=:id_cat LIMIT 5');
-                            $articles1->execute([":id_cat"=>$_GET["cat"]]);
-                    
-                            $resultat_articles1 = $articles1->fetchAll((PDO::FETCH_ASSOC));
+
 
                             // Deuxieme page
 
-                            $articles2 = $sgbd->prepare('SELECT produits.id_produit, categorie.nom, photos.titre, photos.src, photos.alt FROM springfield.photos INNER JOIN springfield.produits ON produits.id_produit = photos.id_produit
+                            $articles2 = $sgbd->prepare('SELECT produits.id_produit, categorie.nom, photos.titre, photos.src, photos.alt 
+                            FROM springfield.photos INNER JOIN springfield.produits ON produits.id_produit = photos.id_produit
                             INNER JOIN categorie ON produits.id_cat = categorie.id_cat WHERE categorie.id_cat=:id_cat LIMIT 5,5;');
                             $articles2->execute([":id_cat"=>$_GET["cat"]]);
                         
-                                $resultat_articles2 = $articles2->fetchAll((PDO::FETCH_ASSOC));
+                            $resultat_articles2 = $articles2->fetchAll((PDO::FETCH_ASSOC));
 
-                            // On dit que si on est sur une page, alors $pg récupere récupere la page
 
-                            $pg=1;
+                    
+                            // On dit que si on est sur une page, alors $pg récupere la page actuelle, sinon il est sur 0
+
+                            $pg=0;
                             if (!empty($_GET['pg'])){
                                 $pg=$_GET['pg'];
                             }
 
-                            // On éxécute les requetes via des boucles pour afficher les cartes ( Need une boucle pour les boucles )
-                            if ($pg==1) {
+                            // Ici on affiche le contenue des pages (les produits) (5 par pages)
+
+
+                            for ($i=0; $i<$nbOfPages; $i++){
+
+                                        $articles = $sgbd->prepare('SELECT produits.id_produit, categorie.nom, photos.titre, photos.src, photos.alt 
+                                        FROM springfield.photos INNER JOIN springfield.produits ON produits.id_produit = photos.id_produit
+                                        INNER JOIN categorie ON produits.id_cat = categorie.id_cat WHERE categorie.id_cat=:id_cat LIMIT 5');
+                                        $articles->execute([":id_cat"=>$_GET["cat"]]);
+                                
+                                        $resultat_articles = $articles->fetchAll((PDO::FETCH_ASSOC));
+
+                                if ($pg==$i) {
+
+                                    foreach ($resultat_articles as $article) {
+
+
+                                        echo    '<figure>
+                                                    <a href="./index.php?ind=desc&desc='.($article['id_produit']).'">
+                                                        <img class="contain" src="data/img/'.($article['src']).'" alt="'.($article['alt']).'">
+                                                        <figcaption>' .($article['titre']). '</figcaption>
+                                                    </a>
+                                                </figure>';
+
+                                            }
+                                        }
+
+
+                            }
+
+
+                            /* if ($pg==0) {
 
                                 foreach ($resultat_articles1 as $article1) {
 
@@ -59,7 +100,7 @@
                                             </figure>';
                                         }
 
-                            } elseif ($pg==2){
+                            } elseif ($pg==1){
 
                                 foreach ($resultat_articles2 as $article2) {
         
@@ -72,7 +113,7 @@
                                         }
 
 
-                            }
+                            } */
 /*
                             foreach ($resultat_articles1 as $article1) {
 
@@ -97,7 +138,6 @@
                 
 */
 
-                     // Boucle for, Si $i = la page en question alors SELECT src alt titre FROM blabla requete au dessus LIMIT 0, 10; 10, 10 pour les autres.
                 ?>
 
     
@@ -106,6 +146,12 @@
 
         </div>
 
+        <?php
+
+
+
+        ?>
+
         <div class="btn">
 
         <div class="arrow">
@@ -113,21 +159,30 @@
         <?php
 
 
+
+            
+
+
                         //Ici on fait une boucle $i qui correspond aux numéros de pages, et on récupere le numéro de page avec $_GET
 
-                for ($i=1; $i<6; $i++){
+                for ($i=0; $i<($nbOfPages); $i++){
                         echo'        
                         <a href="./index.php?ind=cat&cat='.$_GET['cat'].'&pg='.($i).'" class="pages">'.($i).'</a>';
                     };
+
+
+                            
+
+
+
 
         ?>
 
         </div>
 
-
+        </div>
     <?php } ?>
 
-    </div>
 
 
 
