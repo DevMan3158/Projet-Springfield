3704799d2db61eb9bc5ccac77b26b05a823369fd
DevMan3158 <e.giner@codeur.online>
2022-05-17 16:05:01 +0200

    Page descriptif php ok - finitions à prévoir pour image

--- project/www/sgd-admin/src/pages/utilisateur.php
+++ project/www/sgd-admin/src/pages/utilisateur.php
@@ -8,7 +8,7 @@
     //On démarre une nouvelle session
    /* session_start();*/ 
     /*On utilise session_id() pour récupérer l'id de session s'il existe.
-     *Si l'id de session n'existe  pas, session_id() rnevoie une chaine
+     *Si l'id de session n'existe  pas, session_id() renvoie une chaine
      *de caractères vide*/
     /*$id_session = session_id();*/
 
--- project/www/src/pages/descriptif.php
+++ project/www/src/pages/descriptif.php
@@ -1,30 +1,101 @@
 <?php
-include_once './src/bbcode_editeur/conversion_bbcode.php';
-?>
-<p>
-    
-</p>
+ 
 
+
+ //On appel le fichier contenant la conversion BBCODE VERS HTML 
+include_once dirname(__FILE__) . '/../bbcode_editeur/conversion_bbcode.php';
+ //On appel le fichier contenant toutes les infos de la base de données
+include_once dirname(__FILE__) . '/../fonctions/connexion_sgbd.php';
+ //On se connecte à la BDD
+$sgbd= connexion_sgbd();
+ ?>
+ 
+  
+ 
 <div class="contenairTitre">
     <div class="Ligne">
-
     </div>
-    <h3 class="h3Textcenter">Lieux</h3>
-    <div class="Ligne">
+   
+   
+<?php 
+ /* initialise la variable $articles_lbp, pour faire requêtes SQL */
 
+     $articles_lbp = $sgbd->prepare('SELECT produits.nom FROM springfield.produits WHERE produits.id_produit=:id_produit');
+
+
+                        $articles_lbp->execute([":id_produit"=>$_GET["desc"]]);
+
+                        $articles = $articles_lbp->fetchAll(PDO::FETCH_ASSOC);
+
+                        foreach ($articles as $article ){
+                    
+                        echo '<h3 class="h3Textcenter">'.$article['nom'].'</h3>';
+
+                    }
+
+ ?>
+
+    <div class="Ligne">
     </div>
 </div>
 
 <div class="contenairLieux">
 
+         <?php 
+
+        $articles_img = $sgbd->prepare('SELECT photos.id_produit, photos.titre, photos.src, photos.alt ,produits.id_produit FROM springfield.photos INNER JOIN springfield.produits ON photos.id_produit = produits.id_produit WHERE produits.id_produit=:id_produit LIMIT 1; 
+            ');                  
+ /*INNER JOIN categorie ON produits.id_cat = categorie.id_cat*/
+
+        $articles_img->execute([":id_produit"=>$_GET["desc"]]);
+
+                        $articles_I = $articles_img->fetchAll(PDO::FETCH_ASSOC);
+
+                        foreach ($articles_I as $article_i )
+                        {
+          ?>
+
 
     <figure>
+            <?php 
 
+            echo '<img class="recadre" title="'.($article_i['titre']).'" src="data/img/'.($article_i['src']).'" alt="'.($article_i['alt']).'">';  
 
-
+            } ?>                                                                                            
     </figure>
+
+
     <div class="contenairNomImage">
-        <p><?php echo conversion_bbcode("[title]Nom du lieux[/title] \n [b][/b]"); ?></p>
+
+
+
+
+
+        <p>  <!--Appel conversion bbcode pour Nom du lieux, personnages, batiment-->
+            <?php 
+             /* initialise la variable $articles_lbp, pour faire requêtes SQL */
+
+                 $articles_lbp = $sgbd->prepare('SELECT produits.nom FROM springfield.produits WHERE produits.id_produit=:id_produit');
+
+
+                                    $articles_lbp->execute([":id_produit"=>$_GET["desc"]]);
+
+                                    $articles = $articles_lbp->fetchAll(PDO::FETCH_ASSOC);
+
+                                    foreach ($articles as $article ){
+                                    
+                                    
+                                    echo conversion_bbcode('[title]'.$article['nom'].'[/title] [b][/b]');
+                                }
+                            
+             ?>       
+        </p>
+    </div>
+</div>
+
+<div class="transit">
+    <div class="Ligne">
+
     </div>
 </div>
 
@@ -32,70 +103,52 @@ include_once './src/bbcode_editeur/conversion_bbcode.php';
 
 
 
+                <!--Appel conversion bbcode pour la description du lieux, personnages, batiment-->
+    <p>
+
+             <?php 
+                 /* initialise la variable $articles_lbp, pour faire requêtes SQL */
+
+                 $articles_desc = $sgbd->prepare('SELECT produits.description FROM springfield.produits WHERE produits.id_produit=:id_produit; ');
 
 
+                 $articles_desc->execute([":id_produit"=>$_GET["desc"]]);
 
-    
-        
-        <div class="Ligne">
+                 $articles_II = $articles_desc->fetchAll(PDO::FETCH_ASSOC);
 
-        </div>
+                 foreach ($articles_II as $article_ii ){
+                
+                     echo conversion_bbcode($article_ii['description']);
 
-            <h3 class="h3Textcenter">Histoire</h3>
+                    }
 
-        <div class="Ligne">
-
-        </div>   
-
-        <div>
-            <?php echo conversion_bbcode("[title]Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum nisi saepe eveniet
-            fuga perferendis praesentium fugiat delectus. Saepe rem porro veritatis, suscipit,
-            perspiciatis amet, harum nam fugiat ipsa numquam non.[/title] \n [b][/b]"); ?>
-        </div>
-    
-        <div class="Ligne"></div>
-
-        <h3 class="h3Textcenter">Bon à savoir</h3>
-
-        <div class="Ligne"></div>
-
-        <div>
-            <?php echo conversion_bbcode("[title]Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum nisi saepe eveniet
-            fuga perferendis praesentium fugiat delectus. Saepe rem porro veritatis, suscipit,
-            perspiciatis amet, harum nam fugiat ipsa numquam non.[/title] \n [b][/b]"); ?>
-        </div>
-    
-
-        <div class="Ligne"></div> 
-
-        <h3 class="h3Textcenter">Ce qui est inclus</h3>
-        
-        <div class="Ligne"></div>
-
-
-        <div>
-            <?php echo conversion_bbcode("[title]Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum nisi saepe eveniet
-            fuga perferendis praesentium fugiat delectus. Saepe rem porro veritatis, suscipit,
-            perspiciatis amet, harum nam fugiat ipsa numquam non.[/title] \n [b][/b]"); ?>
-        </div>
-    
-
+                ?>       
+    </p>
 
 </div>
 
 
+<div class="transit">
+    <div class="Ligne">
 
+    </div>
+</div>
 
 
 <div class="contenairBtn">
 
-    <input class="favorite styled" type="button" value="SERVICES">
-<input class="favorite styled" type="button" value="HORAIRES ET TARIFS">
-    <input class="favorite styled" type="button" value="CONTACT">
+<a href="./index.php?ind=msg&desc=0"><input class="favorite styled" type="button" value="SERVICES"></a>
+<a href="./index.php?ind=msg&desc=0"><input class="favorite styled" type="button" value="HORAIRES ET TARIFS"></a>
+<a href="./index.php?ind=msg&desc=0"><input class="favorite styled" type="button" value="CONTACT"></a>
 
-    
+
 
 </div>
 
 
-<!--<a href="./index.php?ind=msg&desc=0">Professionel</a>  Qaund users envoie, Envoie au proprietaire avec l'id desc-->
+
+
+
+
+
+
