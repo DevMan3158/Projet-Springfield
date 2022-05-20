48d8480c50d8271c5a8428b6ed161e846e5838c2
DevMan3158 <e.giner@codeur.online>
2022-05-20 14:41:50 +0200

    Page Mentions Légales

--- project/www/src/pages/info.php
+++ project/www/src/pages/info.php
@@ -14,7 +14,7 @@ if($info == "propos") {
     echo file_get_contents(dirname(__FILE__) . '/../template/a_propos.html', true);
 } else if($info == "legales") {
     /* affiche le contenu de la page html a afficher */
-    echo file_get_contents(dirname(__FILE__) . '/../template/mensions_legales.html', true);
+    echo file_get_contents(dirname(__FILE__) . '/../template/mentions_legales.html', true);
 } else if($info == "politique") {
     /* affiche le contenu de la page html a afficher */
     echo file_get_contents(dirname(__FILE__) . '/../template/politique.html', true);
